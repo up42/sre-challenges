f6f7d734f147f5faa67702500858cda00ef1042c
robert.andruschow <robert.andruschow@up42.com>
2021-01-14 11:57:29 +0100

    Add provider.tf

---
+++ dynamic-admission-webhook/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
+
+provider "kubectl" {
+  load_config_file = true
+  config_context_cluster = "minikube"
+}
